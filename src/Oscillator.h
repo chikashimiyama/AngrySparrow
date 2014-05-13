d7dc93b388208c106f3227ac5d1c92448d3e2ddd
Chikashi Miyama <chikashimiyama@gmail.com>
2014-05-13 12:03:00 +0200

    update

--- src/Oscillator.h
+++ src/Oscillator.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @ingroup AngrySparrow
  * @authors Chikashi Miyama
  */
@@ -12,33 +12,41 @@
 namespace AngrySparrow {
     
     
-    /*
+    /**
      * @brief an abstract class of autonomous oscillator
      */
     class Oscillator{
     protected:
-        float frequency;
-        float phase;
-        float phaseIncrement;
+        bool audioRateMode; ///< when true, enables sample based computation (necessary for e.g. frequency modulation )
+        float frequency; ///< frequency in non audioRate mode
+
+        float phase; ///< internal varibale which storing current phase
+        float phaseIncrement; ///< increment by phase
         
-        void updatePhaseIncrement();
-        void getNext();
+        void updatePhaseIncrement(); ///< internal function for calculating phaseIncrement from Freq
+        void getNext(); ///< advance the phase and get the value of phase
     
-        vector<float> frequencyVector;
-        vector<float> outputVector;
+        vector<float> *frequencyVectorPtr; ///< a pointer to a vector of frequencies
+        vector<float> outputVector; ///< output vector
         
+        virtual void performDSP_CR() = 0; ///< abstract function for internal DSP routine for control rate
+        virtual void performDSP_AR() = 0; ///< abstract function for internal DSP routine for audio rate
+
     public:
-        Oscillator(float freq = 440.0, float initialPhase = 0.0);
-        void setFrequency(float frequency);
-        void setFrequency(vector<float> frequencyVector);
-        float getFrequency();
-        float getPhaseIncrement();
+        Oscillator(float freq = 440.0, float initialPhase = 0.0, bool audioRateMode = true); ///< constructor
         
-        void setPhase(float phase);
-        void resetPhase();
-        float getPhase();
+        void setAudioRateMode(bool audioRateMode); ///< set mode of the oscillator
+        bool getAudioRateMode(); ///< get current mode of the osciilator
+        void setFrequency(float frequency); ///< set the control rate frequency
+        void setFrequencyVectorPtr(vector<float> *frequencyVectorPtr); /// set the vector of audio rate frequency
+        float getFrequency(); ///< get the control rate frequency
+        float getPhaseIncrement(); ///< get current phase increment
         
-        virtual vector<float> getNextVector() = 0;
+        void setPhase(float phase); ///< force set the phase
+        void resetPhase(); ///< set the phase to 0
+        float getPhase(); ///< get current phase
+        
+        virtual vector<float> getNextVector() = 0; ///< virtual abstract function for generating audio vectors. should be implmeneted in the subclasses.
     };
     
     inline void Oscillator::updatePhaseIncrement(){
@@ -53,16 +61,29 @@ namespace AngrySparrow {
             phase += CYCLE;
     }
     
-    inline Oscillator::Oscillator(float frequency, float initialPhase){
+    inline Oscillator::Oscillator(float frequency, float initialPhase, bool audioRateMode ){
         Oscillator::phase = initialPhase;
         Oscillator::frequency = frequency;
         updatePhaseIncrement();
         outputVector.assign(vectorSize, 0.0);
+        Oscillator::audioRateMode = audioRateMode;
+    }
+    
+    inline void Oscillator::setAudioRateMode(bool audioRateMode){
+        Oscillator::audioRateMode = audioRateMode;
+    }
+    
+    inline bool Oscillator::getAudioRateMode(){
+        return audioRateMode;
     }
     
     inline void Oscillator::setFrequency(float frequency){
         Oscillator::frequency = frequency;
         updatePhaseIncrement();
+    }
+    
+    inline void Oscillator::setFrequencyVectorPtr(vector<float> *frequencyVectorPtr){
+        Oscillator::frequencyVectorPtr = frequencyVectorPtr;
     }
     
     inline float Oscillator::getFrequency(){
